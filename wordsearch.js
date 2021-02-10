0031d35f3e9966220429203d9a6bd1eed9b6708d
Caitie Garrood <caitlinanneg@gmail.com>
2021-02-10 18:03:58 -0500

    wordsearch finished

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return false;
+  }
+  const colLength = letters.length;
+  const rowLength = letters[0].length;
+  let found = false;
+  for (let col = 0; col < rowLength; col++) {
+    let tempArray = [];
 
-module.exports = wordSearch
+    for (let row = 0; row < colLength; row++) {
+      tempArray.push(letters[row][col]);
+            
+    }
+    const verticalJoin = tempArray.join('');
+    if (verticalJoin.includes(word)) {
+      found = true;
+    }
+  }
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      found = true;
+    }
+  }
+  return found;
+};
+
+module.exports = wordSearch;
